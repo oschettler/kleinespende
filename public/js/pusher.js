f3780ead4948a3eca0c50387c49947e9c30925a3
Olav Schettler <olav@schettler.net>
2016-03-20 00:09:37 +0100

    Pusher JS

---
+++ public/js/pusher.js
@@ -0,0 +1,14 @@
+(function($, pusher) {
+
+    var channel = pusher.subscribe('updateReceiver');
+
+    //channel.bind_all(function(data) {
+    channel.bind('Kleinespende\\Events\\ReceiverUpdated', function(data) {
+        //if (typeof data.receiver !== 'undefined') {
+            $('#open-donations').text(data.receiver.open_donations);
+            $('#month-total-donations').text(data.receiver.month_total_donations);
+            $('#remaining-amount').text(data.receiver.donation_threshold - data.receiver.open_donations);
+        //}
+    });
+
+})(jQuery, pusher);
