7e4bed4e1a24117a8f92be193d1c054a2c36c566
Olav Schettler <olav@schettler.net>
2016-03-20 00:22:16 +0100

    Channel renamed

--- public/js/pusher.js
+++ public/js/pusher.js
@@ -1,6 +1,6 @@
 (function($, pusher) {
 
-    var channel = pusher.subscribe('updateReceiver');
+    var channel = pusher.subscribe('receiverUpdated');
 
     //channel.bind_all(function(data) {
     channel.bind('Kleinespende\\Events\\ReceiverUpdated', function(data) {
